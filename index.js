facd324e3a3d2e726dc21ae9fd87b76a3aebcc1a
vinicius lopes <vinicius.lopes@userede.com.br>
2018-08-26 16:43:45 -0300

    inicio

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ const TOKEN = "cd004f4c385c06e11ed3a44a7aae9acc";
 const GENRES_URL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + TOKEN + "&language=pt-BR";
 let currentDate = moment(new Date()).format("yyyy-MM-dd");
 let currentYear = moment(new Date()).format("yyyy");
-
-const GENRES_MOVIES_URL = "https://api.themoviedb.org/3/discover/movie?api_key=" + TOKEN +
-    "&language=pt-BR&sort_by=popularity.desc&include_adult=true&include_video=false&page=&primary_release_date.gte=" + currentDate + "&year=" + currentYear;
-
-const CITIES_STATE = "https://api-content.ingresso.com/v0/states/";
-const MOVIES_CITIES = "https://api-content.ingresso.com/v0/events/city/"
+const STATE_URL = "https://api-content.ingresso.com/v0/states";
+const CITY_URL = "https://api-content.ingresso.com/v0/events/city/";
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -21,127 +17,61 @@ app.set('views', './app/views');
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/genres', (req, res) => {
-
-    request(GENRES_URL, (error, response, body) => {
-        if (error) {
-            res.status(500);
-            return res.json({
-                message: "Ops! Something is not ok!"
-            });
-        }
-        return res.send(body);
-    });
-});
-
-app.get('/genres/:id', (req, res) => {
-
-    request(GENRES_MOVIES_URL + "&genre=" + req.params.id, (error, response, body) => {
-        if (error) {
-            res.status(500);
-            return res.json({
-                message: "Ops! Something is not ok!"
-            });
-        }
-
-        let movies = body.results.map(function (movie) {
-            return {
-                "title": movie.title,
-                "id": movie.id
-            }
-        });
-
-        return res.send(movies);
-    });
-});
-
-app.get('/cities/:state', (req, res) => {
-
-    request(CITIES_STATE + req.params.state, (error, response, body) => {
-        if (error) {
-            res.status(500);
-            return res.json({
-                message: "Ops! Something is not ok!"
-            });
-        }
-
-        return res.send(body);
-    });
-});
-
-app.get('/movies/:cityId', (req, res) => {
-
-    request(MOVIES_CITIES + req.params.cityId + "/partnership/CINEMARK", (error, response, body) => {
-        if (error) {
-            res.status(500);
-            return res.json({
-                message: "Ops! Something is not ok!"
-            });
-        }
-
-        return res.send(body);
-    });
-});
-
-
-function generos() {
-    return request(GENRES_URL);
-}
 
 app.get('/', (req, res) => {
-    generos()
-        .then(genres => {
-            request("https://api-content.ingresso.com/v0/states", (error, response, body) => {
-                if (error) {
-                    res.status(500);
-                    return res.json({
-                        message: "Ops! Something is not ok!"
-                    });
-                }
 
-                let cities = [];
-                let states = JSON.parse(body);
-                states.forEach(state => {
-                    state.cities.forEach(element => {
-                        cities.push({
-                            "id": element.id,
-                            "urlKey": element.urlKey,
-                            "name": element.name,
-                        })
+    try {
+        request(GENRES_URL)
+            .then(genres => {
+                request(STATE_URL, (error, response, body) => {
+
+                    let cities = [];
+                    let states = JSON.parse(body);
+                    states.forEach(state => {
+                        state.cities.forEach(element => {
+                            cities.push({
+                                "id": element.id,
+                                "urlKey": element.urlKey,
+                                "name": element.name,
+                            })
+                        });
                     });
+                    res.render('index', { genres: JSON.parse(genres).genres, cities: cities });
                 });
-                res.render('index', { genres: JSON.parse(genres).genres, cities: cities });
             });
-        })
+
+    } catch (err) {
+        res.render('index', { genres: [], cities: [] });
+    }
+
 });
 
 
 app.get('/advise', (req, res) => {
-    const cityId = req.query.cityId;
+    let movies = [];
+    try {
+        const cityId = req.query.cityId;
 
-    request("https://api-content.ingresso.com/v0/events/city/"+cityId, (error, response, body) => {
-        if (error) {
-            res.status(500);
-            return res.json({
-                message: "Ops! Something is not ok!"
+        request(CITY_URL + cityId, (error, response, body) => {
+
+            const genre = req.query.genre;
+            let items = JSON.parse(body);
+
+            items.forEach(state => {
+                if (state.genres.indexOf(genre) !== -1) {
+                    movies.push({
+                        "title": state.title,
+                        "city": state.city,
+                        "id": state.id
+                    });
+                }
             });
-        }
-
-        const genre = req.query.genre;
-        let movies = [];
-        let items = JSON.parse(body);
-
-        items.forEach(state => {
-            if(state.genres.indexOf(genre) !== -1){
-                movies.push({
-                    "title": state.title,
-                    "city": state.city,
-                    "id": state.id
-                });
-            }
+            res.render('movies', { movies: movies });
         });
-        res.render('movies', { movies: movies});
-    });
+
+    } catch (err) {
+        res.render('movies', { movies: movies });
+    }
 });
 
 app.listen(PORT, () => {
